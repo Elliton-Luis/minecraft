94d86576fe63e6775794fd5e64a3019b7e38cc9c
Elliton Luis Correia da Silva Junior <ellitonjunior05@gmail.com>
2024-06-12 13:42:29 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+
+// Aplica a todas as apresentações de slides que você define com o HTML escrito
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+    var slides = slideshow.querySelectorAll('[role="list"] .slide'); // Obter um array de slides
+
+    var index = 0, time = 2300;
+    slides[index].classList.add('active');
+
+    setInterval(() => {
+        slides[index].classList.remove('active');
+
+        // Passar por cada slide, incrementando o índice
+        index++;
+
+        // Ao passar por todos os slides, reiniciar o índice para exibir o primeiro slide e iniciar novamente
+        if (index === slides.length) index = 0;
+
+        slides[index].classList.add('active');
+    }, time);
+}
